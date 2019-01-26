c77c07fbd69d9c4da172d0a4b86228a0be77bf06
Virjanand <virjanand@virjanand.com>
2019-01-26 16:00:30 +0100

    Added listview, connected with friends arrayList and made toast on click

--- app/src/main/java/com/virjanand/listviewdemo/MainActivity.java
+++ app/src/main/java/com/virjanand/listviewdemo/MainActivity.java
@@ -2,6 +2,17 @@ package com.virjanand.listviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.friendsListView);
+
+        final ArrayList<String> friends = new ArrayList<>();
+        friends.add("Nobody");
+        friends.add("Lonesome");
+        friends.add("Me");
+        friends.add("Who");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friends);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, friends.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
